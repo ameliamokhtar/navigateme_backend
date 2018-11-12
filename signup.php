49a5f089b1ec7f6a468d36253b4d07a4e8861068
Roslan Logan <roslanlogan94@gmail.com>
2018-11-13 02:09:22 +0800

    fixed signup errors

--- signup.php
+++ signup.php
@@ -10,15 +10,11 @@ $dbname="NavigateMe";
 //create connection
 $conn =  mysqli_connect($servername,$username,$password,$dbname);
 
-$uname = $_POST['username'];
 $pass = $_POST['password'];
 $fn = $_POST['fullname'];
 $r = $_POST['role'];
 $m = $_POST['phone_num'];
 $e = $_POST['email'];
-$p = $_POST['prefix'];
-$lt = $_POST['location_id'];
-$position = $_POST['position'];
 
 //check connection
 if(!$conn){
@@ -31,8 +27,12 @@ $result = mysqli_query($conn,$sql);
 if($result){
     if($r == 1) { //staff
         $id = mysqli_insert_id($conn);
-        $sql_staff = "INSERT INTO staff_information (`user_id`,`full_name`,`phone_num`,`position`,`email`,`prefix`,`location_id`) VALUES 
-        ('$id','$fn','$m','$position','$e','$p','$lt')";
+        $lt = $_POST['location_id'];
+        $add = $_POST['address'];
+        $p = $_POST['prefix'];
+        $position = $_POST['position'];
+        $sql_staff = "INSERT INTO staff_information (`user_id`,`full_name`,`phone_num`,`position`,`email`,`prefix`,`location_id`,`address`) VALUES 
+        ('$id','$fn','$m','$position','$e','$p','$lt','$add')";
         $result_staff = mysqli_query($conn,$sql_staff);
         if($result_staff){
             $response = array('successful' =>true , 'msg' => 'Staff SignUp Succesfully');
@@ -41,8 +41,18 @@ if($result){
         }
     }else if($r == 2){
         $id = mysqli_insert_id($conn);
-        $sql_student = "INSERT INTO student_information (`user_id`,`full_name`, `phone_number`,`prefix`) 
-        VALUES ('$id','$fn','$m','$p')";
+        $sql_student = "INSERT INTO student_information (`user_id`,`full_name`, `phone_number`,`email`) 
+        VALUES ('$id','$fn','$m','$e')";
+        $result_student = mysqli_query($conn,$sql_student);
+        if($result_student){
+            $response = array('successful' =>true , 'msg' => 'Student SignUp Succesfully');
+        }else{
+            $response = array('successful' =>false , 'msg' => 'Student SignUp Unsuccesful');
+        }
+    }else if($r == 3){
+        $id = mysqli_insert_id($conn);
+        $sql_student = "INSERT INTO guest_information (`user_id`,`full_name`, `phone_number`,`email`) 
+        VALUES ('$id','$fn','$m','$e')";
         $result_student = mysqli_query($conn,$sql_student);
         if($result_student){
             $response = array('successful' =>true , 'msg' => 'Student SignUp Succesfully');
